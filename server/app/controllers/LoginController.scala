5eb314a1729dda4abe84cf2519ca538829014157
MorganCartwright2022 <morganjcartwright@gmail.com>
2021-05-04 10:59:33 -0500

    Changed login controller to use an option of user id instead of boolean. Cleaned up login controller.

--- server/app/controllers/LoginController.scala
+++ server/app/controllers/LoginController.scala
@@ -3,16 +3,47 @@ package controllers
 import javax.inject._
 
 import play.api.mvc._
+import play.api.i18n._
 import play.api.libs.json._
-import models.InMemoryLoginModel
+import models.BulletJournalModel
+
 import scala.concurrent._
+import play.api.db.slick._
+import slick.jdbc._
+import slick.jdbc.PostgresProfile.api._
 
 @Singleton
-class LoginController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  import controllers.LoginController._
+class LoginController @Inject()(protected val dbConfigProvider: DatabaseConfigProvider, cc: ControllerComponents)(implicit ec: ExecutionContext) 
+  extends AbstractController(cc) with HasDatabaseConfigProvider[JdbcProfile] {
 
+  import LoginController._
+  private val model = new BulletJournalModel(db)
   implicit val userDataReads = Json.reads[UserData]
-  implicit val ec = ExecutionContext.global
+  
+  def withJsonBody[A](f: A => Future[Result])(implicit request: Request[AnyContent], reads: Reads[A]): Future[Result] = {
+    //Loads Json of type A and calls function with that data
+    request.body.asJson.map { body =>
+      Json.fromJson[A](body) match {
+        case JsSuccess(a, path) => f(a)
+        case e @ JsError(_) => Future.successful(Redirect(routes.LoginController.index()))
+      }
+    }.getOrElse(Future.successful(Redirect(routes.LoginController.index())))
+  }
+
+  def userRequest(requestMethod: (String, String) => Future[Option[Int]])(implicit request: Request[AnyContent]) = {
+    //Processess user validation and creation
+    withJsonBody[UserData]{ userData =>
+      val userIdFutureOption = requestMethod(userData.username, userData.password)
+      userIdFutureOption.map { userIdOption =>
+        userIdOption.map { userId =>
+          Ok(Json.toJson(true))
+            .withSession("username" -> userData.username, "userid" -> userId.toString, "csrfToken" -> play.filters.csrf.CSRF.getToken.map(_.value).getOrElse(""))
+        }.getOrElse {
+          Ok(Json.toJson(false))
+        }
+      }
+    }
+  }
 
   def index = Action { implicit request =>
     //Shows the primary view
@@ -21,40 +52,12 @@ class LoginController @Inject()(cc: ControllerComponents) extends AbstractContro
 
   def validateUser = Action.async { implicit request =>
     //Checks if username and password is valid
-    request.body.asJson.map { body =>
-      Json.fromJson[UserData](body) match {
-        case JsSuccess(input, path) =>
-            val UserData(username, password) = input
-            InMemoryLoginModel.validateUser(username, password).map { validated =>
-              Ok(Json.toJson(validated))
-            }
-        case _ =>
-            println("Error in LoginController validateUser(): invalid JSON data")
-            Future.successful(Ok(Json.toJson(false)))
-      }
-    }.getOrElse {
-      println("Error in LoginController validateUser(): cannot read JSON")
-      Future.successful(Ok(Json.toJson(false)))
-    }
+    userRequest(model.validateUser)
   }
 
   def createUser = Action.async { implicit request =>
     //Creates new username with password
-    request.body.asJson.map { body =>
-      Json.fromJson[UserData](body) match {
-        case JsSuccess(input, path) =>
-            val UserData(username, password) = input
-            InMemoryLoginModel.createUser(username, password).map{ created =>
-              Ok(Json.toJson(created))
-            }
-        case _ =>
-            println("Error in LoginController createUser(): invalid JSON data")
-            Future.successful(Ok(Json.toJson(false)))
-      }
-    }.getOrElse {
-      println("Error in LoginController createUser(): cannot read JSON")
-      Future.successful(Ok(Json.toJson(false)))
-    }
+    userRequest(model.createUser)
   }
 }
 
