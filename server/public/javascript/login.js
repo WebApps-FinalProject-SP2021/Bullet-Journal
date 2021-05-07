5374cc8e3f6fe899024583d1e7555d8d3982187f
mhalle0 <mrosen0@trinity.edu>
2021-05-07 14:52:31 -0500

    fixed non-responding text boxes and label/divider overlap

--- server/public/javascript/login.js
+++ server/public/javascript/login.js
@@ -40,56 +40,60 @@ class LoginComponent extends React.Component {
   
     render() { return(
       ce("div", {className: "container"},
-        ce("div", {className: "row"},
-          ce("form", {className: "col s12 m6 offset-m3"},
-            ce("h3", {className: "center-align"}, "Bullet Journal"),
+          ce("div", {className: "row"},
+            ce("form", {className: "col s12 m6 offset-m3"},
+              ce("h3", {className: "center-align"}, "Bullet Journal"),
+              ce("div", {className: "divider"}),
+              ce("div", {className: "section"},
+                ce("div", {className: "row valign-wrapper"},
+                  ce("div", {className: "input-field col s4", id: "loginNameDiv"},
+                    ce("label", {htmlFor: "loginName"}, "Username"),
+                    ce("input", {type: "text", id: "loginName", value: this.state.loginName, onChange: e => this.changerHandler(e)}),
+                  ),
+                  ce("div", {className: "input-field col s4", id: "loginPassDiv"},
+                    ce("label", {htmlFor: "loginPass"}, "Password"),
+                    ce("input", {type: "password", id: "loginPass", onChange: e => this.changerHandler(e)}),
+                  ),
+                  ce("div", {className: "col s4"},
+                    ce("a", {className: "waves-effect waves-light btn pink lighten-1", onClick: e => this.login(e)}, "Log in"),
+                    ce("span", {id: "login-message"}, this.state.loginMessage),
+                  ),
+                ),
+              ),
             ce("div", {className: "divider"}),
-            ce("div", {className: "row valign-wrapper"},
-              ce("div", {className: "input-field col s4", id: "loginNameDiv"},
-                ce("label", {htmlFor: "loginNameDiv"}, "Username"),
-                ce("input", {type: "text", id: "loginName", value: this.state.loginName, onChange: e => this.changerHandler(e)}),
+            ce("div", {className: "section"},
+              ce("div", {className: "row"},
+                ce("div", {className: "input-field col s12", id: "createNameDiv"},
+                  ce("label", {htmlFor: "createName"}, "Name"),
+                  ce("input", {type: "text", id: "createName", value: this.state.createName, onChange: e => this.changerHandler(e)}),
+                ),
               ),
-              ce("div", {className: "input-field col s4", id: "loginPassDiv"},
-                ce("label", {htmlFor: "loginPassDiv"}, "Password"),
-                ce("input", {type: "password", id: "loginPass", value: this.state.loginPass, onChange: e => this.changerHandler(e)}),
+              ce("div", {className: "row"},
+                ce("div", {className: "input-field col s12", id: "createEmailDiv"},
+                  ce("label", {htmlFor: "createEmail"}, "Email"),
+                  ce('input', {type: "email", id: "createEmail", value: this.state.createEmail, onChange: e => this.changerHandler(e)}),
+                )
               ),
-              ce("div", {className: "col s4"},
-                ce("a", {className: "waves-effect waves-light btn pink lighten-1 onClick: e => this.login(e)"}, "Log in"),
-                ce("span", {id: "login-message"}, this.state.loginMessage),
-              ),
-            ),
-            ce("div", {className: "divider"}),
-            ce("div", {className: "row"},
-              ce("div", {className: "input-field col s6", id: "createNameDiv"},
-                ce("label", {htmlFor: "createNameDiv"}, "Name"),
-                ce("input", {type: "text", id: "createName", value: this.state.createName, onChange: e => this.changerHandler(e)}),
-              ),
-              ce("div", {className: "input-field col s6", id: "createUserNameDiv"},
-                ce("label", {htmlFor: "createUserNameDiv"}, "Username"),
-                ce("input", {type: "text", id: "createUserName", value: this.state.createUserName, onChange: e => this.changerHandler(e)}),
-              )
-            ),
-            ce("div", {className: "row"},
-              ce("div", {className: "input-field col s12", id: "createPassDiv"},
-                ce("label", {htmlFor: "createPassDiv"}, "Password"),
-                ce("input", {type: "password", id: "createPass", value: this.state.createPass, onChange: e => this.changerHandler(e)},
+              ce("div", {className: "row"},
+                ce("div", {className: "input-field inline col s6", id: "createUserNameDiv"},
+                  ce("label", {htmlFor: "createUserName"}, "Username"),
+                  ce("input", {type: "text", id: "createUserName", value: this.state.createUserName, onChange: e => this.changerHandler(e)}),
+                ),
+                ce("div", {className: "input-field inline col s6", id: "createPassDiv"},
+                  ce("label", {htmlFor: "createPass"}, "Password"),
+                  ce("input", {type: "password", id: "createPass", value: this.state.createPass, onChange: e => this.changerHandler(e)}),
                 )
               )
             ),
-            ce("div", {className: "row"},
-              ce("div", {className: "input-field col s12", id: "createEmailDiv"},
-                ce("label", {htmlFor: "createEmailDiv"}, "Email"),
-                ce('input', {type: "email", id: "createEmail", value: this.state.createEmail, onChange: e => this.changerHandler(e)}),
-              )
-            ),
-            ce("a", {className: "waves-effect waves-light btn pink lighten-1 onClick: e => this.createUser(e)"}, "Create Account"),
-            ce('span', {id: "create-message"}, this.state.createMessage)
+              ce("a", {className: "waves-effect waves-light btn pink lighten-1", onClick: e => this.createUser(e)}, "Create Account"),
+              ce('span', {id: "create-message"}, this.state.createMessage)
+            )
           )
         )
-      )
-    );}
+      );}
   
     changerHandler(e) {
+      console.log("changeHandler called on: " + e);
       this.setState({ [e.target['id']]: e.target.value });
     }
   
@@ -109,27 +113,23 @@ class LoginComponent extends React.Component {
       });
     }
 
-    createUser(e)
-    {
-        const fullname = this.state.createName;
-        const username = this.state.createUserName;
-        const password = this.state.createPass;
-        const email = this.state.createEmail;
+    createUser(e) {
+      const fullname = this.state.createName;
+      const username = this.state.createUserName;
+      const password = this.state.createPass;
+      const email = this.state.createEmail;
         fetch(createRoute, {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json', 'Csrf-Token': csrfToken },
-            body: JSON.stringify({ fullname, username, password, email })
-          }).then(res => res.json()).then(data => {
-            if(data) {
-              this.props.doLogin();
-            } else {
-              this.setState({ loginMessage: "Login Failed" });
-            }
-          });
+          method: 'POST',
+          headers: {'Content-Type': 'application/json', 'Csrf-Token': csrfToken },
+          body: JSON.stringify({ fullname, username, password, email })
+        }).then(res => res.json()).then(data => {
+          if(data) {
+            this.props.doLogin();
+          } else {
+             this.setState({ loginMessage: "Login Failed" });
+          }
+        });
     }
-    
-
-    
   }
 
 
