42fddb2d11d24a5f00825e47f2652493c10dee14
Matthew Barton <mbarton@trinity.edu>
2021-05-10 14:21:40 -0500

    Load existing tasks into view

--- server/public/javascript/taskList.js
+++ server/public/javascript/taskList.js
@@ -1,15 +1,32 @@
-const ce = React.createElement
+const ce = React.createElement;
 
+const addTaskRoute = document.getElementById('addTaskRoute').value;
+const allTasksRoute = document.getElementById('allTasksRoute').value;
 
 export class TaskList extends React.Component {
+    _isMounted = false;
+
     constructor(props) {
         super(props);
         this.state = { 
             tasks: [],
-            isShowing: false, 
-            
-        }
+            isShowingDetails: false, 
+            addTitle: "",
+            addDescription: "",
+            addDueDate: "",
+            addReminderDate: ""
+        };
     }
+
+    componentDidMount() {
+        this._isMounted = true;
+        this.loadTasks();
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+      }
+    
 
     render() {
             return( 
@@ -24,8 +41,12 @@ export class TaskList extends React.Component {
                         ce("div", {className: "card"},
                             ce("div", {className: "card-content"},
                                 ce('span', {className: "card-title"}, "List"),
-                                    ce(Task, { show: () => {this.setState({isShowing: !this.state.isShowing})}}, null)
-                     
+                                    //ce(Task, { show: () => this.setState({isShowing: !this.state.isShowing})}, null),
+                                    this.state.tasks.map(task => ce(Task, {key: task.taskid, taskData: task, show: () => this.setState({isShowing: !this.state.isShowingDetails}) }, null)),
+                                    ce("div",{className: "input-field"},
+                                    ce('input', {placeholder:"Title", id: "edit-task"}),
+                                        ce('div', null, "Temp Text")
+                                )
                             )
                         )
                     ),
@@ -43,23 +64,43 @@ export class TaskList extends React.Component {
         
     }
 
+    loadTasks() {
+        fetch(allTasksRoute).then(res => res.json()).then(tasksRet => {if(this._isMounted) this.setState({ tasks: tasksRet })});
+    }
+
+    addTask(e) {
+        fetch(addTaskRoute, { 
+            method: 'POST',
+            headers: {'Content-Type': 'application/json', 'Csrf-Token': csrfToken },
+            body: JSON.stringify(this.state.newTask)
+          }).then(res => res.json()).then(data => {
+            if(data) {
+              this.loadTasks();
+              //this.setState({ taskMessage: "", newTask: "" });
+            } else {
+              //this.setState({ taskMessage: "Failed to add." });
+            }
+          });
+    }
+
     changerHandler(e) {
         this.setState({ [e.target['id']]: e.target.value });
     }
 }
 
 class Task extends React.Component {
+    
     constructor(props) {
         super(props);
         this.state = {
-          title: "Test Title", 
-          description: "Test Description", 
-          completed: "False", 
-          dueDate: "5/9/2021",
-          reminder: "5/8/2021", 
- 
+          title: props.taskData.title, 
+          description: props.taskData.description, 
+          completed: props.taskData.completed, 
+          dueDate: props.taskData.dueDate,
+          reminder: props.taskData.reminder       
         };
     }
+
     render() {return(
         ce("div", {className: "container", onClick: e => this.props.show()}, 
 
@@ -69,10 +110,9 @@ class Task extends React.Component {
                 ce("i", {className: "material-icons circle pink lighten-1"}),
                 ce("span", {className: "title"}, this.state.title),
                 ce("div", null, ce("span", null,  this.state.description)),
-                ce("div", null, ce("span", null,  this.state.dueDate)),
-
-
-                ce("div", null, ce("span", null,  this.state.completed)),
+                this.state.dueDate == undefined ? ce("div", null, ce("span", null,  "No due date set")) : ce("div", null, ce("span", null,  "Due: " + this.state.dueDate)),
+                this.state.reminder == undefined ? ce("div", null, ce("span", null,  "No reminder date set")) : ce("div", null, ce("span", null,  "Reminder: " + this.state.reminder)),
+                this.state.completed ? ce('i', {className: "material-icons"}, 'check_box') : ce('i', {className: "material-icons"}, 'check_box_outline_blank'),
                 ce("a", {className: "secondary-content"}, 
                     ce("i", {className: "material-icons"}),
                 )   
@@ -98,17 +138,17 @@ class EditTask extends React.Component {
             ce("div", {className: "container"},
                 ce("div",{className: "row"},
                     ce("div",{className: "input-field col s6"},
-                        // ce('input ', {placeholder:"tempplaceholder" , id: "edit-task"}),// need to add on chnage handler
+                        ce('input', {placeholder:"tempplaceholder" , id: "edit-task"}),// need to add on chnage handler
                         ce('div', null, "Temp Text" ),
                     ),
                     ce("div",{className: "input-field col s6"},
-                        
+                        'a'
                     ),
                     ce("div",{className: "input-field col s3"},
-                        
+                        'b'
                     ),
                     ce("div",{className: "input-field col s3"},
-                        
+                        'c'
                     ),
                     ce("a", {className: "waves-effect waves-light btn pink lighten-1", onClick: e => this.editTask(e)}, "Save"),
                     ce('span', {id: "edit-task"}, this.state.createMessage)
